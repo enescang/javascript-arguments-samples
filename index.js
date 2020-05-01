f2aef2904b61300d739bdaa726adf6e8d01789fe
kodcanlisi <developerindiee@gmail.com>
2020-05-01 15:34:08 +0300

    Convert to real array and arrow function

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function firstExample() {
   document.getElementById("first").innerHTML = arguments.length;
 }
+
+function secondExample() {
+  for (var i = 0; i < arguments.length; i++) {
+    var node = document.createElement("p"); 
+    var textnode = document.createTextNode(arguments[i]); 
+    node.appendChild(textnode);
+    document.getElementById("second").appendChild(node);
+  }
+}
+
+function convertToArray(){
+    var arr = Array.from(arguments);
+    // or use below line
+    var arr = [...arguments];
+}
+
+/**
+ * In arrow function there is no arguments concept
+ * but there is an other way to get all parameters
+ * inside the function
+ */
+arrowFunction =(...arg)=>{
+    console.log(arg);
+}
